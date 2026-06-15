0dda8011563e6a1f06dbeb8feeee847f82ca4a15
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 20:02:45 -0500

    creating a class
    creating a class with different point of access our variables

--- allocate vector.cpp
+++ allocate vector.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class vec {
+    private:
+    int* arr;
+    int arr_size;
+
+    public:
+
+    vec(int size){
+        arr = new int[size];
+        arr_size = size;
+    }
+
+    ~vec(){
+        delete[] arr;
+    }
+
+    int size(){
+        return arr_size;
+    }
+
+    };
 
 int main(){
+
+    vec v = vec(10);
+
+    int y;
+    cout <<"enter somthing" <,endl;
+    cin >> y;
+    vec v2 = vec(y)
+
 
     return 0;
 }
