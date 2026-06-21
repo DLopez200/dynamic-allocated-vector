cf60be14b53a3deaea28c525e032409f332ba2cb
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-21 13:52:18 -0500

    resize and pushback
    created functions to the class that allowed it to resize when needed and adds elements to the array

--- allocate vector.cpp
+++ allocate vector.cpp
@@ -5,12 +5,27 @@ class vec {
     private:
     int* arr;
     int arr_size;
+    int arr_cap;
+
 
     public:
+    vec(){
+        arr = nullptr;
+        arr_size = 0;
+        arr_cap = 0;
+    }
 
     vec(int size){
+
+        if(size == 0){
+            arr = nullptr;
+            arr_size = 0;
+            arr_cap = 0;
+        }else{
         arr = new int[size];
         arr_size = size;
+        arr_cap = size;
+        }
     }
 
     ~vec(){
@@ -19,6 +34,33 @@ class vec {
 
     int size(){
         return arr_size;
+    }
+
+    int cap(){
+        return arr_cap;
+    }
+
+    void push_back(int x){
+        if(arr_size < arr_cap){
+            arr[arr_size] = x;
+            arr_size++;
+        }
+        else{
+            arr_cap *=2;//doubled the array capacity
+            int* new_arr = new int[arr_cap];
+
+            for(int i = 0; i < arr_size; i++){
+                new_arr[i] = arr[i];
+            }
+            new_arr[arr_size] = x;
+
+            delete[] arr;
+            arr = new_arr;
+            arr_size++;
+
+            cout << "array reseized to " << arr_cap <<endl;
+        }
+        cout <<"pushed back "<< x <<endl;
     }
 
     int& at(int idx){
@@ -35,8 +77,15 @@ class vec {
 
 int main(){
 
-    vec v = vec(10);
+    vec v = vec(1);
 
+    v[0];
+
+    for(int i=0; i <100; i++){
+        v.push_back(i);
+    }
+
+/*
     for(int i =0; i < v.size(); i++){
         v[i] = i;
     }
@@ -50,7 +99,7 @@ int main(){
     cout <<"enter somthing" <<endl;
     cin >> y;
     vec v2 = vec(y);
-
+*/
 
     return 0;
 }
